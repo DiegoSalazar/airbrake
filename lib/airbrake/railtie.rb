9319bb7996c09e9018eceeb7b22a59f09e0f472b
Diego Salazar <diego@greyrobot.com>
2016-10-12 13:56:47 -0400

    fixed rails 5 deprecations

--- lib/airbrake/railtie.rb
+++ lib/airbrake/railtie.rb
@@ -11,19 +11,17 @@ module Airbrake
     end
 
     initializer "airbrake.middleware" do |app|
-
       middleware = if defined?(ActionDispatch::DebugExceptions)
         # Rails >= 3.2.0
-        "ActionDispatch::DebugExceptions"
+        ActionDispatch::DebugExceptions
       else
         # Rails < 3.2.0
         "ActionDispatch::ShowExceptions"
       end
 
-      app.config.middleware.insert_after middleware,
-        "Airbrake::Rails::Middleware"
+      app.config.middleware.insert_after middleware, Airbrake::Rails::Middleware
 
-      app.config.middleware.insert 0, "Airbrake::UserInformer"
+      app.config.middleware.insert 0, Airbrake::UserInformer
     end
 
     config.after_initialize do
